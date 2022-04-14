9ee4dda21f904e8281fb038c6e4b5e52669133f5
Jack Murphy <jackfrancismurphy@outlook.com>
2022-04-14 18:19:06 +0100

    Feature added:  words updates on page if correct

--- newdle_frontend.js
+++ newdle_frontend.js
@@ -1,19 +1,27 @@
 
-const tick_string = "tick for now"
+const TICK_STRING = "tick for now"
 
-const cross_string = "cross for now"
+const CROSS_STRING = "cross for now"
 
+const API_INFO = {headline: "Boris Johnson says tens of thousands could be sent to Rwanda under relocation plan for asylum seekers - UK politics live - The Guardian", scrambled_headline: "Bsrio onnJsoh asys snte fo onduathss uolcd be nset ot awnaRd rneud erolniocta pnla rof mluyas sesekre - UK iosplcti ievl - eTh andiuarG"}
 
+const HEADLINE_ARRAY = API_INFO.headline.split(" ")
+const HEADLINE_ARRAY_LOWER = API_INFO.headline.toLowerCase().split(" ")
+
+const RESULT_PTAG = document.getElementById("result_text")
 
 function checkGuess() {
-    const element = document.getElementById("test_div")
+    const PLAYER_GUESS = document.getElementById("guess_box").value.toLowerCase()
 
-    let player_guess = document.getElementById("guess_box").value.toLowerCase()
-    const answer_array = "Biggest squeeze for public sector pay in 20 years BBC.com".toLowerCase().split(" ")
 
-    if (answer_array.includes(player_guess) == true){
-        element.innerText = tick_string
+    if (HEADLINE_ARRAY_LOWER.includes(PLAYER_GUESS) == true){
+        RESULT_PTAG.innerText = TICK_STRING
+        const GUESS_POSITION = HEADLINE_ARRAY_LOWER.indexOf(PLAYER_GUESS)
+        var updated_scramble = document.getElementById("presented_headline").innerText.split(" ")
+        updated_scramble[GUESS_POSITION] = HEADLINE_ARRAY[GUESS_POSITION]
+        // below we are changing the inner text of the <h2> element on the html file.  
+        document.getElementById("presented_headline").innerText = updated_scramble.join(" ")
     } else {
-        element.innerText = cross_string
+        RESULT_PTAG.innerText = CROSS_STRING
     };
 }
