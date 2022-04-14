a3f6a4104ec1c171bfa76d9e372340f45a2e0e01
Jack Murphy <jackfrancismurphy@outlook.com>
2022-04-14 10:40:44 +0100

    Functionality: players can guess, guess checked

---
+++ page_test.js
@@ -0,0 +1,5 @@
+let player_guess = "dog"
+
+const answer_array = "Biggest squeeze for public sector pay in 20 years BBC.com".split(" ")
+if (answer_array.includes(player_guess) == true){console.log("It's in there")}
+else{console.log("It's not in there")};
